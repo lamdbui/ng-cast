38233e84732b37cbc192ff6bd22f4a4717ab0aeb
Lam Bui <lamdbui@gmail.com>
2017-09-11 17:38:22 -0700

    Fix variable name in videoListEntry to conform to tests

--- src/components/videoListEntry.js
+++ src/components/videoListEntry.js
@@ -1,13 +1,7 @@
 angular.module('video-player')
   .component('videoListEntry', {
     bindings: {
-      item: '<'
-    },
-    controller: function() {
-
-      this.getFormattedUrl = () => {
-        return 'https://i.ytimg.com/vi/OPxeCiy0RdY/default.jpg';
-      };
+      video: '<'
     },
     templateUrl: 'src/templates/videoListEntry.html'
   });
